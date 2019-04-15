0e188d4f70d47c4bf21a5227c83ddf9f0b644c3b
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2019-04-15 16:45:56 +0900

    Create common.js

---
+++ common.js
@@ -0,0 +1,3 @@
+document.getElementsByTag("footer").write('&copy;2019 KisaragiEffective serves under <a href="LICENSE".');
+document.getElementById("github").write('<a href="https://github.com/KisaragiEffective/KisaragiEffective.github.io">FIX ME!</a>');
+document.getElementById("github").write(`<a href="https://github.com/KisaragiEffective/KisaragiEffective.github.io/commits/master/${window.location}">HISTORY</a>`);
