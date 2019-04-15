e5cdd32dc618a2c6fdd891e960feca7097851805
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2019-04-15 17:22:06 +0900

    Update common.js

--- common.js
+++ common.js
@@ -2,5 +2,5 @@ window.addEventListener("load", function() {
   const github = "https://github.com/KisaragiEffective/KisaragiEffective.github.io";
   document.getElementById("footer").write(`&copy;2019 KisaragiEffective serves under <a href="${github}/blob/master/LICENSE">LICENSE</a>.`);
   document.getElementById("github").write(`<a href="${github}">FIX ME!</a>`);
-  document.getElementById("github").write(`<a href="${github}/commits/master/${window.location}">HISTORY</a>`);
+  document.getElementById("github").write(`<a href="${github}/commits/master/${window.location.pathname}">HISTORY</a>`);
 });
