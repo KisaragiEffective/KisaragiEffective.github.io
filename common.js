56b84d749137be2dc99573e94c11095d0b01655a
KisaragiEffective <48310258+KisaragiEffective@users.noreply.github.com>
2019-04-15 16:53:51 +0900

    Update common.js
    fix illegal function

--- common.js
+++ common.js
@@ -1,3 +1,3 @@
-document.getElementsByTag("footer").write('&copy;2019 KisaragiEffective serves under <a href="LICENSE".');
+document.getElementsByTagName("footer").write('&copy;2019 KisaragiEffective serves under <a href="LICENSE".');
 document.getElementById("github").write('<a href="https://github.com/KisaragiEffective/KisaragiEffective.github.io">FIX ME!</a>');
 document.getElementById("github").write(`<a href="https://github.com/KisaragiEffective/KisaragiEffective.github.io/commits/master/${window.location}">HISTORY</a>`);
